6b0bae9330324401a092398edb31d2fa50092836
ninjasaskeh <ariefmaizaki1@gmail.com>
2024-11-26 14:26:17 +0700

    Implement create and read account route

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { ForbiddenError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/valitadion";
+import { APIErrorResponse } from "@/types/global";
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const accounts = await Account.find();
+
+    return NextResponse.json(
+      { success: true, data: accounts },
+      { status: 200 },
+    );
+  } catch (error) {
+    handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+
+    const validatedData = AccountSchema.parse(body);
+
+    const existingUser = await Account.findOne({
+      provider: validatedData.provider,
+      providerAccountId: validatedData.providerAccountId,
+    });
+    if (existingUser)
+      throw new ForbiddenError("An account already exists for this provider");
+
+    const newAccount = await Account.create(validatedData);
+
+    return NextResponse.json(
+      { success: true, data: newAccount },
+      { status: 201 },
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
